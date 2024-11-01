496cb526d1241f8c166ae67679367a34b3e31fa1
Rowfun_bosunia_chowdhury <123250691+Rowfun@users.noreply.github.com>
2024-11-01 19:53:49 +0600

    Initial commit

---
+++ AYTOR/js/app.js
@@ -0,0 +1,17 @@
+$(function(){
+//   BANNER SLIDE SLICK
+    $('#min-banner').slick({
+        autoplay:false,
+        dots:true,
+        arrows:false,
+        autoplaySpeed: 1500,
+      });
+
+
+      $('#banner_sm').slick({
+        arrows:false,
+        autoplay:true,
+        autoplaySpeed:1500,
+      })
+
+})
